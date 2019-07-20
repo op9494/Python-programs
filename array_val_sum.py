8e316247bd1403640de3a9fe8cc9ba302dc26668
op9494 <37392779+op9494@users.noreply.github.com>
2019-07-21 02:22:49 +0530

    Update array_val_sum.py

--- array_val_sum.py
+++ array_val_sum.py
@@ -1,9 +1,8 @@
 n,k=map(int,input().split())
 c=k
-N=[]
 s=0
-for i in range(1,n+1):
-        N.append(i)
+arr=[]
+arr =list(map(int,input().strip().split()))[:n]
 for i in range(0,c):
-  s=s+N[i]
+  s=s+arr[i]
 print(s)
