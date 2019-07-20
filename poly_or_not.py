f961c75e742c0eae681f6ec6fa62d9bbfd3ee195
op9494 <37392779+op9494@users.noreply.github.com>
2019-07-21 02:49:43 +0530

    Update poly_or_not.py

--- poly_or_not.py
+++ poly_or_not.py
@@ -1,14 +1,13 @@
 n=int(input(""))
 t=n
 rev=0
-if n<1000:
-  while(n>0):
+while(n>0):
     dig=n%10
     rev=rev*10+dig
     n=n//10
-  if(t==rev):
-    print("yes")
-  else:
-    print("no")
+if(t==rev):
+  print("yes")
 else:
   print("no")
+ 
+ 
