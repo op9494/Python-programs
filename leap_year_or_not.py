5465f289ccaf066c319202c0d550b2a1dcb7bb39
op9494 <37392779+op9494@users.noreply.github.com>
2019-07-21 00:44:19 +0530

    Update leap_year_or_not.py

--- leap_year_or_not.py
+++ leap_year_or_not.py
@@ -1,11 +1,11 @@
-    y = int(input())  
-    if (y % 4) == 0:  
-       if (y % 100) == 0:  
-           if (y % 400) == 0:  
-               print("yes")  
-           else:  
-               print("no")  
-       else:  
-           print("yes".)  
+y = int(input())
+if (y % 4) == 0:  
+  if (y % 100) == 0:  
+    if (y % 400) == 0:  
+      print("yes")  
     else:  
-       print("no")  
+      print("no")  
+  else:  
+    print("yes")  
+else:  
+  print("no")  
