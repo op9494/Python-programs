79b45ef640b379c2daeebaab2c22da95945266df
op9494 <37392779+op9494@users.noreply.github.com>
2019-07-26 23:15:03 +0530

    Create fib_n_no.py

---
+++ fib_n_no.py
@@ -0,0 +1,15 @@
+nt=int(input())
+n1=1
+n2=1
+c=0
+if nt<=0:
+    00
+elif nt==1:
+   print(n1)
+else:
+   while c<nt:
+       print(n1,end=" ")
+       nth=n1+n2
+       n1=n2
+       n2=nth
+       c+=1
