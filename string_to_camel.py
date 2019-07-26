1d670d3b2e21f2677a869946fd81a447def41e7e
op9494 <37392779+op9494@users.noreply.github.com>
2019-07-27 01:12:52 +0530

    Update string_to_camel.py

--- string_to_camel.py
+++ string_to_camel.py
@@ -1,5 +1,5 @@
 def camel(word):
         import re
-        return ''.join(x.capitalize() or ' ' for x in word.split(' '))
+        return ' '.join(x.capitalize() or ' ' for x in word.split(' '))
 l=input()
 print(camel(l))
