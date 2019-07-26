c69fec7d88d7556cce94f0d21111da9e9add6437
op9494 <37392779+op9494@users.noreply.github.com>
2019-07-27 02:18:22 +0530

    Create H-valu_high.py

---
+++ H-valu_high.py
@@ -0,0 +1,35 @@
+def printMaximum(num): 
+
+ 
+	count = [0 for x in range(10)] 
+
+	
+	string = str(num) 
+
+
+	for i in range(len(string)): 
+		count[int(string[i])] = count[int(string[i])] + 1
+
+	
+	result = 0
+	multiplier = 1
+
+
+	for i in range(10): 
+		while count[i] > 0: 
+			result = result + ( i * multiplier ) 
+			count[i] = count[i] - 1
+			multiplier = multiplier * 10
+
+	
+	return result 
+
+
+n=int(input())
+nu =int(input())
+print (printMaximum(nu)) 
+
+ 
+
+
+
