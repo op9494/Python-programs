e98c82ca11c8194442277a93aecad554d556c513
op9494 <37392779+op9494@users.noreply.github.com>
2019-07-27 11:37:16 +0530

    Update index_and_str_same.py

--- index_and_str_same.py
+++ index_and_str_same.py
@@ -1,6 +1,10 @@
 n=int(input())
 test_list =list(map(int,input().strip().split()))[:n] 
+c=0
 for i in range(len(test_list)): 
   if(i==test_list[i]):
-    print(test_list[i],end=" ") 
-    
+      print(test_list[i],end=" ") 
+  else:
+      c=c+1
+if c==(len(test_list)):
+  print('-1') 
